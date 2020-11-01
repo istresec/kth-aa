2ed8e82d3632f3c95cc09fe2be214818bc88d5a0
Frano Rajič <frano.rajich@gmail.com>
2020-11-01 23:58:29 +0100

    k_nearest_neighbors implemented

--- src/utility.h
+++ src/utility.h
@@ -36,6 +36,16 @@ public:
     T &operator()(size_t row, size_t column) {
         return data[row * _columns + column];
     }
+
+    void print() {
+        for (int r = 0; r < rows(); r++) {
+            for (int c = 0; c < columns(); c++) {
+                cout << (*this)[r][c] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 };
 
 inline double squared_distance(const pair<double, double> &t1, const pair<double, double> &t2) {
@@ -52,6 +62,29 @@ inline Grid<int> *distance_matrix(const vector<pair<double, double>> &cities) {
         }
     }
     return matrix;
+}
+
+inline Grid<int> *k_nearest_neighbors(const vector<pair<double, double>> &cities, Grid<int> &distances, int k) {
+    auto knn = new Grid<int>(cities.size(), k);
+
+    auto cmp = [](const pair<int, int> &a, const pair<int, int> &b) { return a.first > b.first; };
+    auto heap = vector<pair<int, int>>();
+    heap.reserve(cities.size());
+
+    for (unsigned i = 0; i < cities.size(); i++) {
+        heap.clear();
+        for (unsigned j = 0; j < cities.size(); j++) {
+            if (i != j) { heap.emplace_back(distances[i][j], j); }
+        }
+        make_heap(heap.begin(), heap.end(), cmp);
+
+        for (int neighbor = 0; neighbor < k; neighbor++) {
+            (*knn)[i][neighbor] = heap.front().second;
+            pop_heap(heap.begin(), heap.end(), cmp);
+            heap.pop_back();
+        }
+    }
+    return knn;
 }
 
 #endif //KTH_AA_UTILITY_H
